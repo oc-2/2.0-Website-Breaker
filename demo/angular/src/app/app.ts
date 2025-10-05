89de773eb1c445f61c5f64c4314f62bd289231f4
oc-2 <185859974+oc-2@users.noreply.github.com>
2025-10-05 18:32:19 +0000

    Done

--- demo/angular/src/app/app.ts
+++ demo/angular/src/app/app.ts
@@ -1,11 +1,13 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { BreakButtonComponent } from './break-button/break-button.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  standalone: true,
+  imports: [RouterOutlet, BreakButtonComponent],
   templateUrl: './app.html',
-  styleUrl: './app.sass'
+  styleUrl: './app.css'
 })
 export class App {
   protected readonly title = signal('website-breaker');
